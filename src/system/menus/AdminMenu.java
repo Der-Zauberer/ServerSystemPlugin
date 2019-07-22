c0018a9371c7b5c0f33c178bdc9388e56ae2573b
Der-Zauberer <and.som282@gmail.com>
2019-07-22 18:37:08 +0200

    Update InventoryMenu system, because GitHub didn't push

---
+++ src/system/menus/AdminMenu.java
@@ -0,0 +1,35 @@
+package system.menus;
+
+import org.bukkit.Color;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffectType;
+
+import system.utilities.InventoryMenu;
+
+public class AdminMenu extends InventoryMenu{
+
+	public AdminMenu(Player player) {
+		super(player, 36, "Admin");
+		addItem("Gamemode Survival", Material.IRON_SHOVEL, 0);
+		addItem("Gamemode Creative", Material.IRON_PICKAXE, 9);
+		addItem("Gamemode Adventure", Material.IRON_SWORD, 18);
+		addItem("Gamemode Spectator", Material.IRON_HELMET, 27);
+		addItem("Time Morning", Material.CLOCK, 2);
+		addItem("Time Day", Material.CLOCK, 11);
+		addItem("Time Night", Material.CLOCK, 20);
+		addItem("Time Midnight", Material.CLOCK, 29);
+		addItem("Effect Speed", Color.BLUE, PotionEffectType.SPEED, 4);
+		addItem("Effect Jump Boost", Color.GREEN, PotionEffectType.JUMP, 13);
+		addItem("Effect Invisibilitiy", Color.PURPLE, PotionEffectType.INVISIBILITY, 22);
+		addItem("Effect Clear", Material.GLASS_BOTTLE, 31);
+		addItem("Weather Clear", Material.SUNFLOWER, 6);
+		addItem("Weather Rain", Material.WATER_BUCKET, 15);
+		addItem("Weather Thunderstorm", Material.HOPPER, 24);
+		addItem("Server Settings", Material.SKELETON_SKULL, 8);
+		addItem("World Setting", Material.ZOMBIE_HEAD, 17);
+		addItem("Players", Material.CREEPER_HEAD, 35);
+		open();
+	}
+
+}
---
+++ src/system/utilities/InventoryMenu.java
@@ -0,0 +1,57 @@
+package system.utilities;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class InventoryMenu implements Listener{
+	
+	private Inventory inventory;
+	private Player player;
+	
+	public InventoryMenu(Player player, int number,  String name) {
+		this.player = player;
+		inventory = Bukkit.createInventory(player, number, name);
+	}
+	
+	public ItemStack addItem(String name, Material material, int position) {
+		ItemStack itemstack = new ItemStack(material);
+		ItemMeta itemmeta = itemstack.getItemMeta();
+		itemmeta.setDisplayName(name);
+		itemstack.setItemMeta(itemmeta);
+		inventory.setItem(position, itemstack);
+		return itemstack;
+	}
+	
+	public ItemStack addItem(String name, Color color, PotionEffectType effect, int position) {
+		ItemStack itemstack = new ItemStack(Material.POTION);
+		PotionMeta potionmeta = (PotionMeta) itemstack.getItemMeta();
+		potionmeta.setDisplayName(name);
+		potionmeta.setColor(color);
+		potionmeta.addCustomEffect(new PotionEffect(effect, 3600, 2), true);
+		itemstack.setItemMeta(potionmeta);
+		inventory.setItem(position, itemstack);
+		return itemstack;
+	}
+	
+	public void open() {
+		player.openInventory(inventory);
+	}
+
+	public Inventory getInventory() {
+		return inventory;
+	}
+
+	public void setInventory(Inventory inventory) {
+		this.inventory = inventory;
+	}
+
+}
