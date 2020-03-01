5c1afcf3fef4e5936caea18e98b4676c4db27bd6
Der-Zauberer <and.som282@gmail.com>
2020-03-01 12:09:33 +0100

    Made eventhandling for ExtraItems possible

--- src/serversystem/handler/ExtraItemHandler.java
+++ src/serversystem/handler/ExtraItemHandler.java
@@ -1,11 +1,12 @@
 package serversystem.handler;
 
 import java.util.ArrayList;
+
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-
-import net.md_5.bungee.api.ChatColor;
 import serversystem.utilities.ExtraItem;
 
 public class ExtraItemHandler implements Listener {
@@ -27,7 +28,7 @@ public class ExtraItemHandler implements Listener {
 	
 	public static ExtraItem getExtraItemByName(String name) {
 		for (ExtraItem extraitem : extraitems) {
-			if(extraitem.getId().equals(name)) {
+			if(extraitem.getDisplayName().equals(name)) {
 				return extraitem;
 			}
 		}
@@ -36,7 +37,12 @@ public class ExtraItemHandler implements Listener {
 	
 	@EventHandler
 	public void onInteract(PlayerInteractEvent event) {
-		
+		if(event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_AIR) {
+			String itemname = ChatColor.stripColor(event.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName());
+			if(getExtraItemByName(itemname) != null) {
+				getExtraItemByName(itemname).onItemInteract(event, event.getPlayer());
+			}
+		}
 	}
 	
 }
