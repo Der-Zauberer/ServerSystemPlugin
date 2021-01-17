c9616cc9527054a0b78ae13d116ee269a183bdd1
Der-Zauberer <and.som282@gmail.com>
2021-01-17 10:39:57 +0100

    Improved permission command

--- src/serversystem/commands/PermissionCommand.java
+++ src/serversystem/commands/PermissionCommand.java
@@ -8,7 +8,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
-
 import serversystem.config.Config;
 import serversystem.handler.ChatHandler;
 import serversystem.handler.PlayerPermission;
@@ -19,44 +18,53 @@ public class PermissionCommand implements CommandExecutor, TabCompleter{
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if(args[0].equals("group")) {
-			if(args[1].equals("add") && args[2] != null) {
-				if(!Config.getSection("Groups").contains(args[2])) {
-					Config.addGroup(args[2]);
-					ChatHandler.sendServerMessage(sender, "The group " + args[2] + " is added!");
-				} else {
-					ChatHandler.sendServerErrorMessage(sender, "The group " + args[2] + " is already added!");
-				}
-			}
-			if(args[1].equals("remove") && args[2] != null) {
-				if (Config.getSection("Groups").contains(args[2])) {
-					Config.removeGroup(args[2]);
-					ChatHandler.sendServerErrorMessage(sender, "The group " + args[2] + " is removed!");
-				} else {
-					ChatHandler.sendServerErrorMessage(sender, "The group " + args[2] + " does not exist!");
-				}
-				
-			}
-		}
-		if(args[0].equals("player")) {
-			if(args[1] != null && args[2] != null && Bukkit.getPlayer(args[1]) != null) {
-				if(Bukkit.getOnlinePlayers().contains(Bukkit.getPlayer(args[1]))) {
-					if(Config.getSection("Groups").contains(args[2])) {
-						Player player = Bukkit.getPlayer(args[1]);
-						PlayerPermission.removeConfigPermissions(player);
-						Config.setPlayerGroup(player, args[2]);
-						PlayerPermission.addConfigPermissions(player);
-						PlayerPermission.reloadPlayerPermissions(player);
-						TeamHandler.addRoleToPlayer(player);
-						ChatHandler.sendServerMessage(sender, "The player " + args[1] + " is in group " + args[2] + " now!");
+		System.out.println(args.length);
+		if(args.length == 3) {
+			if(args[0].equals("group")) {
+				if(args[1].equals("add") && args[2] != null) {
+					if(!Config.getSection("Groups").contains(args[2])) {
+						Config.addGroup(args[2]);
+						ChatHandler.sendServerMessage(sender, "Added the group " + args[2] + "!");
+					} else {
+						ChatHandler.sendServerErrorMessage(sender, "The group " + args[2] + " is already added!");
+					}
+				}else if(args[1].equals("remove") && args[2] != null) {
+					if (Config.getSection("Groups").contains(args[2])) {
+						Config.removeGroup(args[2]);
+						ChatHandler.sendServerMessage(sender, "Removed the group " + args[2] + "!");
 					} else {
 						ChatHandler.sendServerErrorMessage(sender, "The group " + args[2] + " does not exist!");
 					}
+					
 				} else {
-					ChatHandler.sendServerErrorMessage(sender, "The player " + args[0] + " is not online!");
+					ChatHandler.sendServerErrorMessage(sender, "The option " + args[1] + " does not exist!");
 				}
-				
+			} else if(args[0].equals("player")) {
+				if(args[1] != null && args[2] != null) {
+					if(Bukkit.getPlayer(args[1]) != null && Bukkit.getOnlinePlayers().contains(Bukkit.getPlayer(args[1]))) {
+						if(Config.getSection("Groups").contains(args[2])) {
+							Player player = Bukkit.getPlayer(args[1]);
+							PlayerPermission.removeConfigPermissions(player);
+							Config.setPlayerGroup(player, args[2]);
+							PlayerPermission.addConfigPermissions(player);
+							PlayerPermission.reloadPlayerPermissions(player);
+							TeamHandler.addRoleToPlayer(player);
+							ChatHandler.sendServerMessage(sender, "Moved the player " + args[1] + " in group " + args[2] + "!");
+						} else {
+							ChatHandler.sendServerErrorMessage(sender, "The group " + args[2] + " does not exist!");
+						}
+					} else {
+						ChatHandler.sendServerErrorMessage(sender, "The player " + args[0] + " is not online!");
+					}
+					
+				} else {
+					ChatHandler.sendServerErrorMessage(sender, "Not enought arguments!");
+				}
+			} else {
+				ChatHandler.sendServerErrorMessage(sender, "The option " + args[0] + " does not exist!");
 			}
+		} else { 
+			ChatHandler.sendServerErrorMessage(sender, "Not enought arguments!");
 		}
 		return true;
 	}
@@ -73,7 +81,7 @@ public class PermissionCommand implements CommandExecutor, TabCompleter{
 			commands.clear();
 			commands.add("add");
 			commands.add("remove");
-		} else if(args.length == 3 && args[0].equals("group") && (args[1].equals("add") || args[1].equals("remove"))) {
+		} else if(args.length == 3 && args[0].equals("group") && args[1].equals("remove")) {
 			commands.clear();
 			commands = Config.getSection("Groups");
 		} else if(args.length == 3 && args[0].equals("player")) {
@@ -90,4 +98,4 @@ public class PermissionCommand implements CommandExecutor, TabCompleter{
 		return commands;
 	}
 	
-	}
+}
