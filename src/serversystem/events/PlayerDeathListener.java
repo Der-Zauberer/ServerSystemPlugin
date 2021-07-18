f2bbfc0b1fecfa748f9c446c2b5e880442bbbfce
Der-Zauberer <and.som282@gmail.com>
2021-07-18 22:54:57 +0200

    Fixed deathmessage

--- src/serversystem/events/PlayerDeathListener.java
+++ src/serversystem/events/PlayerDeathListener.java
@@ -1,7 +1,6 @@
 package serversystem.events;
 
 import org.bukkit.World;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -13,13 +12,20 @@ public class PlayerDeathListener implements Listener {
 	
 	@EventHandler
 	public void onDeath(PlayerDeathEvent event) {
-		if(event.getEntity() instanceof Player && WorldGroupHandler.isEnabled()) {
-			World world = event.getEntity().getWorld();
-			if(Config.hasWorldDeathMessage(world.getName())) {
-				ChatHandler.sendServerWorldGroupMessage(WorldGroupHandler.getWorldGroup(event.getEntity()), event.getDeathMessage());
+		World world = event.getEntity().getWorld();
+		if(Config.hasWorldDeathMessage(world.getName())) {
+			String message = event.getDeathMessage();
+			if(Config.getPlayerGroup(event.getEntity()) != null && Config.getGroupPrefix(Config.getPlayerGroup(event.getEntity())) != null && message.startsWith(Config.getGroupPrefix(Config.getPlayerGroup(event.getEntity())))) {
+				message = message.substring(Config.getGroupPrefix(Config.getPlayerGroup(event.getEntity())).length());
 			}
-			WorldGroupHandler.playerdeaths.put(event.getEntity(), world);
+			if(WorldGroupHandler.isEnabled()) {
+				ChatHandler.sendServerWorldGroupMessage(WorldGroupHandler.getWorldGroup(event.getEntity()), message);
+			} else {
+				ChatHandler.sendServerBroadcastMessage(message);
+			}
+			
 		}
+		WorldGroupHandler.playerdeaths.put(event.getEntity(), world);
 		event.setDeathMessage("");
 	}
 	
